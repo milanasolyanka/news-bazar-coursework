cb0746a753792d6df93fe4f010ce9412c08dd88b
Zlobin Nikita <zlobin.n.s@yandex.ru>
2023-05-12 05:59:03 +0700

    Create app.py
    Бэкенд

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+import requests
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/news', methods=['GET'])
+def get_news():
+    api_key = '238131f7f6664e22b6d625ac06847c72'  # Вставьте ваш API-ключ здесь
+    url = 'https://newsapi.org/v2/top-headlines'
+    parameters = {
+        'country': 'ua',
+        'apiKey': api_key
+    }
+
+    response = requests.get(url, params=parameters)
+
+    return jsonify(response.json()), 200
+
+if __name__ == "__main__":
+    app.run(port=5000)
