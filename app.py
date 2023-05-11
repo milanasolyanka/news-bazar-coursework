5714ff5470846d1206b1f2d484d851b2d8032a6f
Zlobin Nikita <zlobin.n.s@yandex.ru>
2023-05-12 06:06:12 +0700

    Update app.py
    Добавлен Flask сервер для подключения к NewsAPI. Сервер получает новости из США и возвращает их в формате JSON. CORS настроен для обеспечения кросс-доменных запросов.

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
+# Импорт нужных библиотек
 from flask import Flask, jsonify, request
 import requests
 from flask_cors import CORS
 
+# Константы, которые можно легко изменить
+API_KEY = '238131f7f6664e22b6d625ac06847c72'
+DEFAULT_URL = 'https://newsapi.org/v2/top-headlines'
+DEFAULT_COUNTRY = 'us'
+
+# Создание Flask приложения
 app = Flask(__name__)
+
+# Настройка CORS для приложения
 CORS(app)
 
 @app.route('/news', methods=['GET'])
 def get_news():
-    api_key = '238131f7f6664e22b6d625ac06847c72'  # Вставьте ваш API-ключ здесь
-    url = 'https://newsapi.org/v2/top-headlines'
+    # Параметры для запроса к News API
     parameters = {
-        'country': 'ua',
-        'apiKey': api_key
+        'country': DEFAULT_COUNTRY,
+        'apiKey': API_KEY
     }
 
-    response = requests.get(url, params=parameters)
+    # Отправка запроса к News API
+    response = requests.get(DEFAULT_URL, params=parameters)
 
+    # Возвращение ответа от News API в формате JSON с кодом статуса 200
     return jsonify(response.json()), 200
 
+# Запуск Flask приложения, если скрипт запущен напрямую
 if __name__ == "__main__":
     app.run(port=5000)
